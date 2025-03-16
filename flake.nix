d79921e42fafee1a7e21c1e28bc9916ca5b1adea
Sam Title <samstitle@gmail.com>
2025-03-16 19:33:12 -0400

    Got the example running with an nginx server

--- flake.nix
+++ flake.nix
@@ -40,6 +40,54 @@
             cp dist/index.html $out
           '';
         };
+
+        docker = pkgs.dockerTools.buildImage {
+          name = "zero-to-nix-javascript";
+          tag = "latest";
+
+          copyToRoot = pkgs.buildEnv {
+            name = "image-root";
+            paths = [ pkgs.nginx ];
+          };
+
+          extraCommands = ''
+            mkdir -p var/log/nginx
+            mkdir -p var/cache/nginx
+          '';
+
+          runAsRoot = ''
+            #!${pkgs.stdenv.shell}
+            ${pkgs.dockerTools.shadowSetup}
+            groupadd --system nginx
+            useradd --system --gid nginx nginx
+          '';
+
+          config = {
+            Cmd = [ "nginx" "-c" (pkgs.writeText "nginx.conf" ''
+              user nginx nginx;
+              daemon off;
+              error_log /dev/stdout info;
+              pid /dev/null;
+              events {
+                worker_connections 1024;
+              }
+              http {
+                access_log /dev/stdout;
+                server {
+                  listen 80;
+                  index index.html;
+                  location / {
+                    root ${self.packages.${pkgs.system}.zero-to-nix-javascript};
+                  }
+                }
+              }
+            '') ];
+            ExposedPorts = {
+              "80/tcp" = {};
+            };
+          };
+        };
+
         default = self.packages.${pkgs.system}.zero-to-nix-javascript;
       });
 
@@ -48,6 +96,21 @@
           type = "app";
           program = toString (pkgs.writeShellScript "open-result" ''
             ${pkgs.xdg-utils}/bin/xdg-open ${self.packages.${pkgs.system}.zero-to-nix-javascript}/index.html
+          '');
+        };
+
+        run-container = {
+          type = "app";
+          program = toString (pkgs.writeShellScript "run-container" ''
+            ${pkgs.docker}/bin/docker load < ${self.packages.${pkgs.system}.docker}
+            ${pkgs.docker}/bin/docker run -p 8080:80 zero-to-nix-javascript:latest
+          '');
+        };
+
+        stop-container = {
+          type = "app";
+          program = toString (pkgs.writeShellScript "stop-container" ''
+            ${pkgs.docker}/bin/docker stop $(${pkgs.docker}/bin/docker ps -q --filter ancestor=zero-to-nix-javascript:latest)
           '');
         };
       });
